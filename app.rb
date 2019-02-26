bf66ff259785b7b445c132cc6387e0886c9c237c
Jane Venter <janeanneventer@gmail.com>
2019-02-26 05:04:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,11 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    
+    get '/' do 
+      erb :'pirates/index'
+    end
+    
     get '/new' do 
       erb :'pirates/new'
     end

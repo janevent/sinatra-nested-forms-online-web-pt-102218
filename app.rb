7235b6436aec9938649433055bf641f7f50ce073
Jane Venter <janeanneventer@gmail.com>
2019-02-26 05:19:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ module FormsLab
     
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
-      
+      binding.pry
       params[:pirate][:ships].each do |details|
         Ship.new(details)
       end
